bc808afb36fee19f2e886908a2d1a643944a60a9
DAniel OGwok <danogwok@yahoo.co.uk>
2019-08-12 18:40:24 +0200

    fixes

--- myModule.c
+++ myModule.c
@@ -1,31 +1,38 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
+#include <linux/module.h>           // Core header for loading LKMs into the kernel
+#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
 
-MODULE_LICENCSE("GPL");
-MODULE_AUTHOR("Daniel Ogwok");
-MODULE_DESCRIPTION("A basic Linux kernel module  that supports module parameters");
-MODULE_VERSION("0.1");
+MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
+MODULE_AUTHOR("Daniel Ogwok");      ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A Basic Linux Kernel Module that supports module parameters.");  ///< The description -- see modinfo
+MODULE_VERSION("0.1");              ///< The version of the module
 
-static char *name = "world";	//An eample LKM argument - default value is "world"
-module_param(name, charp, S_IRUG0);	//Parameter Description character pointer, charp = char ptr, S_IRUG0 can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");	//Parameter description
+static char *name = "world";        ///< An example LKM argument -- default value is "world"
+module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
 
-/**
- * LKM Initialization function
+/** @brief The LKM initialization function
+ *  The static keyword restricts the visibility of the function to within this C file. The __init
+ *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
+ *  time and that it can be discarded and its memory freed up after that point.
+ *  @return returns 0 if successful
  */
 static int __init myModule_init(void){
-	printk(KERN_INFO "myModule: Hello %, from my Module LKM. \n", name);
-	return 0;
+   printk(KERN_INFO "myModule: Hey %s from my first LKM!\n", name);
+   return 0;
 }
 
-/**
- * LKM cleanup function
+/** @brief The LKM cleanup function
+ *  Similar to the initialization function, it is static. The __exit macro notifies that if this
+ *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
 static void __exit myModule_exit(void){
-	printk(KERN_INFO "myModule: Goodbye %s from my Module LKM. \n", name);
+   printk(KERN_INFO "myModule: Goodbye %s from my first LKM!\n", name);
 }
 
+/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
+ *  identify the initialization function at insertion time and the cleanup function (as
+ *  listed above)
+ */
 module_init(myModule_init);
-module_init(myModule_exit);
-
+module_exit(myModule_exit);
